f2bd7928699a14e21f6f220ec59d4856d48e03ab
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-09-14 11:42:27 -0400

    added land sat 8 sr cloud mask

--- cwi/server/cmasking.py
+++ cwi/server/cmasking.py
@@ -117,3 +117,19 @@ class S2CloudlessBuilder:
 
     def build(self):
         return self
+
+
+def mask_l8_sr(image: ee.Iamge) -> ee.Image:
+    qaMask = image.select('QA_PIXEL').bitwiseAnd(int('11111', 2)).eq(0);
+    saturationMask = image.select('QA_RADSAT').eq(0);
+
+    # Apply the scaling factors to the appropriate bands.
+    opticalBands = image.select('SR_B.').multiply(0.0000275).add(-0.2);
+    thermalBands = image.select('ST_B.*').multiply(0.00341802).add(149.0);
+
+    
+    return (image.addBands(opticalBands, None, True) 
+            .addBands(thermalBands, None, True)
+            .updateMask(qaMask)
+            .updateMask(saturationMask)
+        )
